cae13635d083cdfdaa0947c91c25a006aeed2f98
rawan ali <rawan.08.ali@gmail.com>
2022-10-03 23:04:05 +0300

    the site with dynamic loading

---
+++ after/js/module4task/script2.js
@@ -0,0 +1,42 @@
+// (function (window){
+
+
+// 	var zozo1 = {};
+
+// 	zozo1.name = "zozo";
+
+// 	rand = "hi";
+
+// 	zozo1.sayhi = function (){
+
+// 		console.log ( rand + zozo1.name);
+
+// 	}
+
+//    window.zozo1 = zozo1;
+
+// })(window);
+
+
+ var speakeword = {};
+
+ speakeword.name = "Hello";
+
+
+ speakeword.speak = function(name){
+  // function speak (name){ 
+
+
+	console.log(speakeword.name + name);
+}
+
+
+
+
+
+
+
+
+
+
+
